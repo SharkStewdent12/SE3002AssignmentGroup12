dc73f9abf4347e5250583b1e6a517d31af240842
Mark Durrheim <570169@students.wits.ac.za>
2014-06-01 18:49:00 +0200

    now reads from our textfile

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from peewee import *
 
 db = SqliteDatabase('people.db')
+inputFile = open('lab3Input.txt','r')
 
 class Person(Model):
 	name = CharField()
@@ -11,15 +12,24 @@ class Person(Model):
 
 
 #Main
+
+
+
 if Person.table_exists():
 	Person.drop_table()
 
 Person.create_table()
 
-for i in range(0,15):
-	Person.create( name = 'test'+str(i), stuNum = i+100)
+#for i in range(0,15):
+#	Person.create( name = 'test'+str(i), stuNum = i+100)
+
+for record in inputFile:
+	line =  record.split(',')
+	recNum = int( line[0].strip() )
+	recName = line[1].strip()
+	Person.create( name = recName, stuNum = recNum)
 
 for person in Person.select():
-	print person.name, person.stuNum
+	print person.stuNum, person.name
 
 
