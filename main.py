5195d6ea5baf1a5d8b991b4a14b466736bf71031
Mark Durrheim <570169@students.wits.ac.za>
2014-06-07 21:35:02 +0200

    half of program transferred into funcions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from peewee import *
 
 db = SqliteDatabase('people.db')
-inputFile = open('lab3Input.txt','r')
+
 outputFile = open('lab3Output.txt','w')
 
 class Person(Model):
@@ -11,29 +11,43 @@ class Person(Model):
 	class Meta:
 		database = db
 
+def initialiseDatabase():
+	if Person.table_exists():
+		Person.drop_table()
+	Person.create_table()
+	
+def readToDB(textfileName):
+	inputFile = open(textfileName,'r')
+	for record in inputFile:
+		line =  record.split(',')
+		recNum = int( line[0].strip() )
+		recName = line[1].strip()
+		Person.create( name = recName, stuNum = recNum)
 
+def getPeopleFromDB():
+	persons = []
+	for person in Person.select():
+		persons.append(person)
+	return persons
+	
+def sortPeople(persons, order):
+	persons = sorted(persons, key=lambda person:person.stuNum, reverse = order)
+	return persons
+	
 #Main
-if Person.table_exists():
-	Person.drop_table()
 
-Person.create_table()
+initialiseDatabase()
 
-for record in inputFile:
-	line =  record.split(',')
-	recNum = int( line[0].strip() )
-	recName = line[1].strip()
-	Person.create( name = recName, stuNum = recNum)
+readToDB('lab3Input.txt')
 
-for person in Person.select():
-	print person.stuNum, person.name
 
-#Now for reading for output
+persons = getPeopleFromDB()
 
-persons = []
-for person in Person.select():
-	persons.append(person)
-
-persons = sorted(persons, key=lambda person:person.stuNum)
+#get flag for order
+ASC = False
+DESC = True
+order = ASC
+persons = sortPeople(persons,order)
 
 for person in persons:
 	print '-',person.stuNum, person.name
