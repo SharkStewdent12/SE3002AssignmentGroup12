d259f9724c00208974557e8579975d67450b49cc
Helix_Nebula <matt.unterslak@gmail.com>
2014-06-09 11:24:00 +0200

    Test program finalised.

--- src/testProgram.py
+++ src/testProgram.py
@@ -2,3 +2,48 @@
 from dbFunctions import *
 
 #do stuff!
+try:
+    initialiseDatabase()
+    print "Database Initialised"
+except:
+    print "Database Initialisation Failed"
+
+try:
+    readToDB('Broken.txt')
+    print "Textfile reading into database Passed"
+except:
+    print "Textfile contains errors. Test Failed."
+
+try:
+    printSampleFromDB()
+except:
+    print "Textfile contains less than 30 entries or blank entries encountered. Test Failed."
+    
+try:
+    persons = getPeopleFromDB()
+    print "Database reading Passed"
+except:
+    print "Database reading Failed. "
+   
+try:
+    order = getOrderFlag()
+    print "Order from user correct"
+except:
+    print "Order not of type expected. "
+
+try:
+    persons = sortPeople(persons,order)
+    print "Sort entries Passed"
+except:
+    print "Sort Failed. "
+
+try:
+    writePeopleToText(persons,'lab3Output.txt')
+    print "Writing to output file Passed"
+except:
+    print "Writing to output file Failed."
+
+try:
+    printSampleFromText('lab3Output.txt')
+except:
+    print "The database contains less than 30 entries or blank entries encountered. Print Failed."
