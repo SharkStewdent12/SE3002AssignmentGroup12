9739dc25327422334f64e1d07fde0c8b72ba808f
Helix_Nebula <matt.unterslak@gmail.com>
2014-06-09 14:33:57 +0200

    Minor bug fixes

--- src/testProgram.py
+++ src/testProgram.py
@@ -1,51 +1,63 @@
-#Written by Matthew Unterslak 
+#Written by Matthew Unterslak
 from dbFunctions import *
 
 try:
-    initialiseDatabase()
-    print "Database Initialised"
+	initialiseDatabase()
+	print "Database Initialised"
 except:
-    print "Database Initialisation Failed"
+	print "Database Initialisation Failed"
 
 try:
-    readToDB('Broken.txt')
-    print "Textfile reading into database Passed"
+	readToDB('Broken.txt')
+	print "Textfile reading into database Passed"
 except SystemExit:
-	print "Error caught"
+	print "Error caught because of System Exit"
 except:
-    print "Textfile contains errors. Test Failed."
+	print "Textfile contains errors. Test Failed."
 
 try:
-    printSampleFromDB()
-    print "Print sample Passed"
+	printSampleFromDB()
+	print "Print sample Passed"
+except SystemExit:
+	print "Error caught because of System Exit"
 except:
-    print "Textfile contains less than 30 entries or blank entries encountered. Test Failed."
+	print "Textfile contains less than 30 entries or blank entries encountered. Print Sample Failed."
 try:
-    persons = getPeopleFromDB()
-    print "Database reading Passed"
+	persons = getPeopleFromDB()
+	print "Database reading Passed"
+except SystemExit:
+	print "Error caught because of System Exit"
 except:
-    print "Database reading Failed."
+	print "Database reading Failed."
    
 try:
-    order = getOrderFlag()
-    print "Order from user Passed."
+	order = getOrderFlag()
+	print "Order from user Passed."
+except SystemExit:
+	print "Error caught because of System Exit"
 except:
-    print "Order not of type expected. Order from user Failed"
+	print "Order not of type expected. Order from user Failed"
 
 try:
-    persons = sortPeople(persons,True)
-    print "Sort entries Passed"
+	persons = sortPeople(persons,True)
+	print "Sort entries Passed"
+except SystemExit:
+	print "Error caught because of System Exit"
 except:
-    print "Sort Failed."
+	print "Sort Failed."
 
 try:
-    writePeopleToText(persons,'lab3Output.txt')
-    print "Writing to output file Passed"
+	writePeopleToText(persons,'lab3Output.txt')
+	print "Writing to output file Passed"
+except SystemExit:
+	print "Error caught because of System Exit"
 except:
-    print "Writing to output file Failed."
+	print "Writing to output file Failed."
 
 try:
-    printSampleFromText('lab3Output.txt')
-    print "Print sample from Text Passed"
+	printSampleFromText('lab3Output.txt')
+	print "Print sample from Text Passed"
+except SystemExit:
+	print "Error caught because of System Exit"
 except:
-    print "The database contains less than 30 entries or blank entries encountered. Print Failed."
+	print "The database contains less than 30 entries or blank entries encountered. Print Failed."
